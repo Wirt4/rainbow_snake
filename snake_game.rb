c9b6a434ec24aef1cb802cb2925bd1e467fbde70
Wirt4 <wirt.salthouse@gmail.com>
2019-12-31 16:14:15 -0800

    collision detection seems to be diverting from the tutorial example

--- snake_game.rb
+++ snake_game.rb
@@ -10,15 +10,14 @@ class Snake
   attr_writer :direction
 
   def initialize
-    @position = [[16, 21], [16, 22], [16,23]]
+    @position = [[16, 21], [16, 22], [16, 23]]
     @direction = 'up'
+    @growing = false
   end
 
   def draw
-    # Square.new(x: 2* GRID_SIZE, y: 0, size: GRID_SIZE, color: 'maroon')
-    #
     @position.each do |pos|
-      Square.new(x: pos[0]* GRID_SIZE, y: pos[1]* GRID_SIZE, size: GRID_SIZE, color: 'maroon')
+      Square.new(x: pos[0] * GRID_SIZE, y: pos[1] * GRID_SIZE, size: GRID_SIZE, color: 'maroon')
     end
   end
   def get_dir
@@ -28,7 +27,9 @@ class Snake
     @direction = dir
   end
   def move
-    @position.shift
+    unless @growing
+      @position.shift
+    end
     case @direction
     when 'down'
       @position.push(new_cords(head[0], head[1]+1))
@@ -39,10 +40,27 @@ class Snake
     when 'right'
       @position.push(new_cords(head[0] + 1, head[1]))
     end
+    @growing = false
   end
 
   def head
     @position.last
+  end
+
+  def x
+    head[0]
+  end
+
+  def y
+    head[1]
+  end
+
+  def grow
+    @growing = true
+  end
+
+  def collision? #collision detection is not working as expected
+    @position.uniq.length == @position.length
   end
 
   private
@@ -51,13 +69,57 @@ class Snake
   end
 end
 
+class Game
+  def initialize
+    @score = 0
+    @food_x = rand(GRID_WIDTH) # may want a second method to spawn food to not to interfere with snake
+    @food_y = rand(GRID_HEIGHT)
+    @finished = false
+  end
+  def draw
+    unless finished?
+      Square.new(x: @food_x* GRID_SIZE, y: @food_y * GRID_SIZE, size: GRID_SIZE, color: 'fuchsia')
+      end
+    Text.new("Score: #{@score}", color: 'black', x: 10, y: 10, size: 25)
+  end
+
+  def snake_eat_food?(x, y)
+    @food_x == x && @food_y == y
+  end
+
+  def record_hit
+    @score +=1
+    @food_x = rand(GRID_WIDTH)
+    @food_y = rand(GRID_HEIGHT)
+  end
+
+  def finish
+    @finished = true
+  end
+
+  def finished?
+    @finished
+  end
+end
+
 snake = Snake.new
+game = Game.new
 snake.draw
 update do
   #the cycle
   clear
-  snake.move
+  snake.move #unless game.finished?
   snake.draw
+  game.draw
+
+  if game.snake_eat_food?(snake.x, snake.y)
+    game.record_hit
+    snake.grow
+  end
+
+  if snake.collision?
+    game.finish
+  end
 end
 
 on :key_down do |event|
